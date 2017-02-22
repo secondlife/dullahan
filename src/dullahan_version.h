a55412f00c6225ed15ce2640cc5889dab91e1241
Callum Prentice <callum@gmail.com>
2017-02-22 12:21:34 -0800

    Bump version number to track new features/changes

--- src/dullahan_version.h
+++ src/dullahan_version.h
@@ -30,6 +30,6 @@
 
 static const int DULLAHAN_VERSION_MAJOR = 1;
 static const int DULLAHAN_VERSION_MINOR = 1;
-static const int DULLAHAN_VERSION_BUILD = 605;
+static const int DULLAHAN_VERSION_BUILD = 606;
 
 #endif // _DULLAHAN_VERSION
